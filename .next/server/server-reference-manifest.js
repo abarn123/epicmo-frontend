50d95b11b4417fb9e1ccc2de856f4608379701dc
abarn123 <albar123cmh@gmail.com>
2025-07-20 10:21:51 -1200

    Remove .next from repository tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v7rGfPGQWjWA8iWNpbyLIgbXpTBs3PtcMbwD3YZyc0A=\"\n}"
--- .next/static/chunks/app_user_page_tsx_568cd5ea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_b592b6f1._.js",
-  "static/chunks/node_modules_afc22f62._.js"
-],
-    source: "dynamic"
-});
