d25cbce7587538a41f34f33abe30f5b155de14d9
Zxess <ardirafif142@gmail.com>
2025-07-21 10:24:40 +0700

    Remove .next folder from tracking ardi

--- .next/static/chunks/app_log_tools_page_tsx_568cd5ea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_616be197._.js",
-  "static/chunks/app_da58964d._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_user_edit_page_tsx_568cd5ea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_692b3ff1._.js",
-  "static/chunks/app_user_edit_page_tsx_1416b86b._.js"
-],
-    source: "dynamic"
-});
